0c359c00849a0e2c994858bfa598b36ad92c53a7
Disath Liyanage <disathsenitha@gmail.com>
2026-04-14 11:48:54 +0530

    FCP & SI opt

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/outfit/latin-400.css'
-import '@fontsource/outfit/latin-700.css'
-import '@fontsource/space-grotesk/latin-400.css'
-import '@fontsource/space-grotesk/latin-700.css'
 import './index.css'
 import App from './App.jsx'
 import NotFound from './pages/NotFound.jsx'
 
 const isHomeRoute = window.location.pathname === '/' || window.location.pathname === '/index.html'
+
+function loadFonts() {
+  import('@fontsource/outfit/latin-400.css')
+  import('@fontsource/outfit/latin-700.css')
+  import('@fontsource/space-grotesk/latin-400.css')
+  import('@fontsource/space-grotesk/latin-700.css')
+}
+
+if ('requestIdleCallback' in window) {
+  window.requestIdleCallback(loadFonts, { timeout: 1200 })
+} else {
+  window.setTimeout(loadFonts, 250)
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
