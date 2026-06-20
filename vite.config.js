f5b606be4276bc09b083b9ae4eaa64de8011c6ed
Disath Liyanage <disathsenitha@gmail.com>
2026-06-21 02:01:44 +0530

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
+import fs from "fs";
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({ hostname: 'https://disathliyanage.com' })
+
+    {
+      name: "create-dist-folder",
+      closeBundle() {
+        if (!fs.existsSync("dist")) {
+          fs.mkdirSync("dist");
+        }
+      },
+    },
+
+    Sitemap({
+      hostname: "https://yourdomain.com",
+    }),
   ],
-})
+});
